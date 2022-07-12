ee22beb1255cf1c0ea13433732dea6b2f018b03e
ItsKarthikVinod <karthivinu111222@gmail.com>
2022-07-12 19:53:21 +0400

    footer target-blank issue resolved

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,16 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="flex justify-center items-center gap-3 py-3">
-      <a href="https://www.youtube.com/c/KarthikVinod9" target="_blank">
+      <a
+        href="https://www.youtube.com/c/KarthikVinod9"
+        target="_blank"
+        rel="noreferrer"
+      >
         <i className="fa-brands fa-youtube duration-300 hover:opacity-30 cursor-pointer"></i>
       </a>
-      <h1 className="text-sm sm:text-base ">Established by Karthik Vinod 🙂 </h1>
+      <h1 className="text-sm sm:text-base ">
+        Established by Karthik Vinod 🙂{" "}
+      </h1>
     </div>
   );
 }
